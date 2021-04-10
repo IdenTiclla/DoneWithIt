56d70a29c95ede9816c5440f06e74e490ab9f098
unknown <iden.ticlla@gmail.com>
2021-04-10 09:20:09 -0400

    add alert component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, Alert } from 'react-native';
 
 export default function App() {
     console.log("App executed")
@@ -11,6 +11,18 @@ export default function App() {
         <View style={styles.container}>
             <Text numberOfLines={3} onPress={handlePress}>Hello React Native A really long text. NOw i wanna make this even more more long text</Text>
             <Button color="orange" title="click me" onPress={()=> console.log("button clicked")}></Button>
+            <Button 
+                color="blue" 
+                title="click me" 
+                onPress={()=> Alert.alert("My title", "My Message",[
+                    {text:"YEES!", onPress: () => console.log("clickeaste yeees")},
+                    {text:"NOOO!", onPress: () => console.log("clickeaste nooo")},
+                ])}></Button>
+
+            <Button 
+                color="red" 
+                title="click me prompt" 
+                onPress={()=> Alert.prompt("My Title", "my message", (text) => console.log(text))}></Button>
             <StatusBar style="auto" />
         </View>
     );
