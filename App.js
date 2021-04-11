d52922f9d3270883acff6211d0c1c0ad2878a08c
unknown <iden.ticlla@gmail.com>
2021-04-11 08:04:12 -0400

    dimensions

--- App.js
+++ App.js
@@ -1,31 +1,49 @@
 
 import React from 'react';
 
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, StatusBar, Alert, Platform } from 'react-native';
+import { 
+    Dimensions,
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    TouchableHighlight,
+    Button,
+    StatusBar,
+    Alert, 
+    Platform 
+} from 'react-native';
 
 export default function App() {
     console.log("App executed")
+    console.log(Dimensions.get("screen"))
     
     const handlePress = () => console.log("Text pressed")
 
     return (
-        <View style={[styles.container, containerStyle]}>
-            <Text numberOfLines={3} onPress={handlePress}>Hello React Native A really long text. NOw i wanna make this even more more long text</Text>
-            <Button color="orange" title="click me" onPress={()=> console.log("button clicked")}></Button>
-            <Button 
-                color="blue" 
-                title="click me" 
-                onPress={()=> Alert.alert("My title", "My Message",[
-                    {text:"YEES!", onPress: () => console.log("clickeaste yeees")},
-                    {text:"NOOO!", onPress: () => console.log("clickeaste nooo")},
-                ])}></Button>
+        <SafeAreaView style={[styles.container, containerStyle]}>
+            <View style={{
+                backgroundColor:'dodgerblue',
+                width:'50%',
+                height:500
+            }}>
+                <Text numberOfLines={3} onPress={handlePress}>Hello React Native A really long text. NOw i wanna make this even more more long text</Text>
+                <Button color="orange" title="click me" onPress={()=> console.log("button clicked")}></Button>
+                <Button 
+                    color="blue" 
+                    title="click me" 
+                    onPress={()=> Alert.alert("My title", "My Message",[
+                        {text:"YEES!", onPress: () => console.log("clickeaste yeees")},
+                        {text:"NOOO!", onPress: () => console.log("clickeaste nooo")},
+                    ])}></Button>
 
-            <Button 
-                color="red" 
-                title="click me prompt" 
-                onPress={()=> Alert.prompt("My Title", "my message", (text) => console.log(text))}></Button>
-            <StatusBar style="auto" />
-        </View>
+                <Button 
+                    color="red" 
+                    title="click me prompt" 
+                    onPress={()=> Alert.prompt("My Title", "my message", (text) => console.log(text))}></Button>
+            </View>
+        </SafeAreaView>
     );
 }
 
