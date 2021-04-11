11174c76c81ae4ac217d0df384a8362c7936f462
unknown <iden.ticlla@gmail.com>
2021-04-11 17:14:10 -0400

    Flex direction

--- App.js
+++ App.js
@@ -30,24 +30,35 @@ export default function App() {
 
     return (
         <View style={{
-            backgroundColor: "dodgerblue",
-            flex: 1
+            paddingTop: StatusBar.currentHeight,
+            flex: 1,
+            flexDirection: "row"
+            
         }}>
             <View style={{
                 backgroundColor: "dodgerblue",
-                flex:1,
-            }}/>
+                width: 100,
+                height: 100
+            }}>
+                <Text>Hello world</Text>
+            </View>
 
             <View style={{
                 backgroundColor: "gold",
-                flex:1,
-            }}/>
+                width: 100,
+                height: 100
+            }}>
+                <Text>Hello world</Text>
+            </View>
 
 
             <View style={{
                 backgroundColor: "tomato",
-                flex:1,
-            }}/>
+                width: 100,
+                height: 100
+            }}>
+                <Text>Hello world</Text>
+            </View>
 
         </View>
     );
